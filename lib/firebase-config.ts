7e9a28ce9a3c7d695dede9a72fe2e881c73b3302
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-12-27 20:55:00 +0100

    chore: firebase temporary update

--- lib/firebase-config.ts
+++ lib/firebase-config.ts
@@ -5,12 +5,12 @@ import { Firestore, getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_ID,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLICFIREBASE_STORAGE_BUCKET,
+  apiKey: "AIzaSyCrTf5wyJYX1QWtrcyduHAjDHxm3lU_ks4",
+  authDomain: "dudurewa-super-admin.firebaseapp.com",
+  projectId: "dudurewa-super-admin",
+  storageBucket: "dudurewa-super-admin.appspot.com",
+  messagingSenderId: "263290400322",
+  appId: "1:263290400322:web:044ed65a6a7ef326f6c7db"
 };
 
 
